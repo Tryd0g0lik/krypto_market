b87b6c7d786cdb60c877374870e32480c48a86d8
Denis <work80@mail.ru>
2026-02-02 12:44:01 +0700

    [dev]: THe task 'task_record_user_encrypt_key' was rename on the 'task_caching_user_data'

--- cryptomarket/tasks/queues/task_user_encrypt_key.py
+++ cryptomarket/tasks/queues/task_user_encrypt_key.py
@@ -7,44 +7,58 @@ import base64
 import json
 import logging
 
-import backoff
-from celery.bin.result import result
-
 from cryptomarket.project.enums import RadisKeysEnum
+
+#
+# import backoff
+# from celery.bin.result import result
+
 
 log = logging.getLogger(__name__)
 
 
-async def task_record_user_encrypt_key(
+async def task_caching_user_data(
+    *args,
     **kwargs,
 ) -> None:
     """
+    :param args: [< ticker_name >]
     :param kwargs: {< client_id: str > : json.dumps([ ... ])}
     :param kwargs[client_id]: (str) The client id of the user. This client index from the deribit account.
         That is the required variable!
     """
-    if kwargs is None or not isinstance(kwargs, dict):
+    if (
+        args is None
+        or not isinstance(args, list)
+        or kwargs is None
+        or not isinstance(kwargs, dict)
+    ):
         raise TypeError(
-            "%s TypeError => The 'kwargs' type is not successful!",
-            (task_record_user_encrypt_key.__name__,),
+            "%s TypeError => The type 'args' or 'kwargs' variable is incorrect!",
+            (task_caching_user_data.__name__,),
         )
-    if len(kwargs) < 1 or kwargs.get("client_id") is None:
+    if (
+        args[0] is None
+        or not isinstance(args[0], str)
+        or len(kwargs) < 1
+        or kwargs.get("client_id") is None
+    ):
         raise ValueError(
-            "%s ERROR => The 'kwargs' value is not defined or 'kwargs[client_id]' was not found!",
-            (task_record_user_encrypt_key.__name__,),
+            "%s ERROR => The 'args' is incorrect or 'kwargs' value is not defined or 'kwargs[client_id]' was not found!",
+            (task_caching_user_data.__name__,),
         )
     try:
-        await get_record(**kwargs)
+        await get_record(*args, **kwargs)
     except asyncio.TimeoutError as e:
         log.error(
             "%s TimeoutError => %s"
-            % (task_record_user_encrypt_key.__name__, e.args[0] if e.args else str(e))
+            % (task_caching_user_data.__name__, e.args[0] if e.args else str(e))
         )
 
     except Exception as e:
         log.error(
             "%s TypeError => %s"
-            % (task_record_user_encrypt_key.__name__, e.args[0] if e.args else str(e))
+            % (task_caching_user_data.__name__, e.args[0] if e.args else str(e))
         )
 
 
@@ -55,18 +69,19 @@ async def task_record_user_encrypt_key(
 #         max_time=5,
 #     )
 async def get_record(
+    *args,
     **kwargs,
 ) -> None:
     from cryptomarket.deribit_client import DeribitLimited
 
     deribit_limited = DeribitLimited()
     context_redis_connection = deribit_limited.context_redis_connection
-    redis_key = RadisKeysEnum.AES_REDIS_KEY.value % kwargs["client_id"]
+
     try:
 
         async with context_redis_connection() as redis_client:
             result_ = await asyncio.wait_for(
-                redis_client.setex(redis_key, 97200, json.dumps(kwargs)), 10
+                redis_client.setex(args[0], 97200, json.dumps(kwargs)), 10
             )
             return result_
     except Exception as e:
